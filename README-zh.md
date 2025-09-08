7483c8c6c1caca07f9722072121d610163f50428
crazyyanchao <yanchaoma@foxmail.com>
2025-09-08 23:16:13 +0800

    chore: update package.json with author details and dependency versions

---
+++ README-zh.md
@@ -0,0 +1,2 @@
+
+nodes\VolcengineAi\VolcengineAi.node.ts 这个文件记录了一个坑注意一下
---
+++ nodes/VolcengineAi/VolcengineAi.node.ts
@@ -0,0 +1,59 @@
+// import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
+// import { NodeConnectionTypes } from 'n8n-workflow';
+// import { chatFields, chatOperations } from './ChatDescription';
+// import { FIMFields, fimOperations } from './FIMDescription';
+
+// /*
+// 这个节点加载时我遇到过一问题：类名不能加`node`后缀，否则n8n加载时会报错，例如 VolcengineAiNode 这是不行的 修改为 VolcengineAi 就可以了
+// */
+// export class VolcengineAi implements INodeType {
+// 	description: INodeTypeDescription = {
+// 		displayName: 'VolcEngine AI',
+// 		name: 'volcengineAi',
+// 		// hidden: true,
+// 		icon: { light: 'file:volcengine.logo.svg', dark: 'file:volcengine.logo.svg' },
+// 		group: ['transform'],
+// 		version: 1,
+// 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+// 		description: 'Consume VolcEngine AI',
+// 		defaults: {
+// 			name: 'VolcEngine AI',
+// 		},
+// 		inputs: [NodeConnectionTypes.Main],
+// 		outputs: [NodeConnectionTypes.Main],
+// 		credentials: [
+// 			{
+// 				name: 'volcengineAiApi',
+// 				required: true,
+// 			},
+// 		],
+// 		requestDefaults: {
+// 			ignoreHttpStatusErrors: true,
+// 			baseURL: 'https://ark.cn-beijing.volces.com/api/v3',
+// 		},
+// 		properties: [
+// 			{
+// 				displayName: 'Resource',
+// 				name: 'resource',
+// 				type: 'options',
+// 				noDataExpression: true,
+// 				options: [
+// 					{
+// 						name: 'Chat',
+// 						value: 'chat'
+// 					},
+// 					{
+// 						name: 'FIM',
+// 						value: 'fim'
+// 					}
+// 				],
+// 				default: 'chat',
+// 			},
+
+// 			...chatOperations,
+// 			...fimOperations,
+// 			...chatFields,
+// 			...FIMFields
+// 		],
+// 	};
+// }
